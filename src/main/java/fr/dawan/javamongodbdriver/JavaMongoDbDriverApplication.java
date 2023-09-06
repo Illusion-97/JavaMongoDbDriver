708c78738a2438b25bce764839c85740ceb37ac5
Yanis Adekalom <yanis971.60@gmail.com>
2023-09-06 09:18:12 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/javamongodbdriver/JavaMongoDbDriverApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.javamongodbdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongoDbDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMongoDbDriverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dawan/javamongodbdriver/JavaMongoDbDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.javamongodbdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongoDbDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
