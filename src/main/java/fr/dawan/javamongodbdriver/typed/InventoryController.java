5306666a5eb29b921d2ad69df9392fd9f8289187
Yanis Adekalom <yanis971.60@gmail.com>
2023-09-06 13:55:12 +0200

    Feat (Web) : InventoryController > FindAll + Paginated + ByItem

--- src/main/java/fr/dawan/javamongodbdriver/typed/InventoryController.java
+++ src/main/java/fr/dawan/javamongodbdriver/typed/InventoryController.java
@@ -7,13 +7,12 @@ import com.mongodb.client.result.InsertOneResult;
 import fr.dawan.javamongodbdriver.conf.ConnexionManager;
 import fr.dawan.javamongodbdriver.typed.documents.Inventory;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -45,5 +44,30 @@ public class InventoryController {
     public InsertManyResult insertMany(@RequestBody List<Inventory> inventories) {
         // db.inventory.insertMany([{},{}])
         return collection.insertMany(inventories);
+    }
+
+    @GetMapping
+    public List<Inventory> findAll() {
+        // var result = db.inventory.find({})
+        // result.forEach(printjson)
+        /*List<Inventory> result = new ArrayList<>();
+        //collection.find().forEach(inventory -> result.add(inventory));
+        collection.find().forEach(result::add);
+        return result;*/
+        return collection.find().into(new ArrayList<>());
+    }
+
+    @GetMapping("/allPaginated")
+    public List<Inventory> findAllPaginated(@RequestParam int page, @RequestParam int size) {
+        // db.inventory.find({}).skip(page * size).limit(size)
+        return collection.find().skip(page * size).limit(size).into(new ArrayList<>());
+    }
+
+    @GetMapping("findByItem/{item}")
+    public List<Inventory> findByItem(@PathVariable String item) {
+        // db.inventory.find({key: value})
+        Document document = new Document();
+        document.append("item",item);
+        return collection.find(document).into(new ArrayList<>());
     }
 }
